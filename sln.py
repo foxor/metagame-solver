762e3daf934e2c85ad63475b3a5f52f5512c70e0
Isaac James <izach.james@gmail.com>
2014-06-21 17:53:31 +0000

    File format

--- sln.py
+++ sln.py
@@ -2,44 +2,33 @@
 
 import numpy as np
 from scipy import linalg
+import sys
 
-matchup_input = {
-  'R': {'R': 0.5, 'P': 0.2, 'S': 0.6},
-  'P': {'R': 0.8, 'P': 0.5, 'S': 0.3},
-  'S': {'R': 0.4, 'P': 0.7, 'S': 0.5}
-}
-
-_name_indexes = []
+_strategies = []
 _matchups = []
 
-def parse_input():
-  global _name_indexes, _matchups
+def parse_input(fName):
+  global _strategies, _matchups
 
-  _name_indexes = list(matchup_input.keys())
-  name_lookup = dict((_name_indexes[i], i) for i in xrange(len(_name_indexes)))
+  fptr = open(fName, 'r')
 
-  _matchups = np.array(
-    list(
-      list(matchup_pair[1] for matchup_pair in 
-        sorted(
-          list((matchup, matchup_input[strategy][matchup]) for matchup in matchup_input[strategy]),
-          key= lambda matchup_pair: name_lookup[matchup_pair[0]]
-        )
-      ) for strategy in _name_indexes
-    )
-  )
-  import pdb;pdb.set_trace()
+  num_strategies = int(fptr.readline().strip())
+  _strategies = list(fptr.readline().strip() for x in xrange(num_strategies))
 
-  
-  #_matchups = np.array([
-  #  [0.5, 0.2, 0.6],
-  #  [0.8, 0.5, 0.3],
-  #  [0.4, 0.7, 0.5]
-  #])
+  _matchups = np.identity(num_strategies) * 0.5
+
+  line_index = 0
+  for matchup_line in (fptr.readline() for x in xrange(num_strategies - 1)):
+    match_index = line_index + 1
+    for matchup in (float(x) for x in matchup_line.strip().split(' ')):
+      _matchups[line_index][match_index] = matchup
+      _matchups[match_index][line_index] = 1.0 - matchup
+      match_index += 1
+    line_index += 1
 
 def viability_check():
   """
-  The idea here is to remove all strategies that are strictly worse than another strategy are removed
+  The idea here is to remove all strategies that are strictly worse than another strategy
   """
   pass
 
@@ -48,9 +37,15 @@ def solve():
 
 def display(solutions):
   for i in xrange(len(solutions)):
-    print "%s: %.2f%%" % (_name_indexes[i], solutions[i] * 100.0)
+    print "%s: %.2f%%" % (_strategies[i], solutions[i] * 100.0)
 
-if __name__ == '__main__':
-  parse_input()
+def main(fName):
+  parse_input(fName)
   viability_check()
   display(solve())
+
+if __name__ == '__main__':
+  if len(sys.argv) == 2:
+    main(sys.argv[1])
+  else:
+    print "Usage: %s <filename>" % sys.argv[0]
